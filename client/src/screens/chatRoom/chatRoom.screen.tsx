8dfcdbc94aef25b7ae316f054e680b5fe865ea19
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-04-16 16:28:21 +0200

    add controll btns

--- client/src/screens/chatRoom/chatRoom.screen.tsx
+++ client/src/screens/chatRoom/chatRoom.screen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import useStyles from './chatRoom.styles';
+import { Fab } from '@material-ui/core';
+import {
+	MicRounded,
+	VideocamRounded,
+	ScreenShareRounded,
+	ChatRounded,
+	PictureInPictureRounded,
+	CallEndRounded
+} from '@material-ui/icons';
 
 // components
 import Header from './components/Header/Header';
 import Videos from './components/videos/videos';
 import ChatEntire from './components/chatEntire/chatEntire';
+
+import useStyles from './chatRoom.styles';
 
 const ChatRoomScreen: React.FC = () => {
 	const styles = useStyles();
@@ -14,6 +24,31 @@ const ChatRoomScreen: React.FC = () => {
 			<Header />
 			<Videos remoteVideoText="Waiting for other user to join..." />
 			<ChatEntire />
+			<div className={styles.controlButtons}>
+				<Fab size="medium" color="default" aria-label="toggle-microphone" className={styles.controlButton}>
+					<MicRounded />
+				</Fab>
+				<Fab size="medium" color="default" aria-label="toggle-video" className={styles.controlButton}>
+					<VideocamRounded />
+				</Fab>
+				<Fab size="medium" color="default" aria-label="shear-screen" className={styles.controlButton}>
+					<ScreenShareRounded />
+				</Fab>
+				<Fab size="medium" color="default" aria-label="toggle-chat" className={styles.controlButton}>
+					<ChatRounded />
+				</Fab>
+				<Fab
+					size="medium"
+					color="default"
+					aria-label="toggle-picture-in-picture"
+					className={styles.controlButton}
+				>
+					<PictureInPictureRounded />
+				</Fab>
+				<Fab size="medium" color="default" aria-label="call-end" className={styles.controlButton}>
+					<CallEndRounded />
+				</Fab>
+			</div>
 		</div>
 	);
 };
--- client/src/screens/chatRoom/chatRoom.styles.ts
+++ client/src/screens/chatRoom/chatRoom.styles.ts
@@ -22,4 +22,24 @@ export default makeStyles({
     animationDuration: '0.3s',
     overflow: 'hidden',
   },
+
+  controlButtons: {
+    position: 'absolute',
+    left: 'calc(7.5vw - 40px)',
+    top: '50%',
+    transform: 'translate(0%, -50%)',
+    zIndex: 999,
+    borderRadius: '10px',
+    background: '#16171a',
+    boxShadow: '9px 9px 16px #0a0b0c, -9px -9px 16px #222328',
+    padding: 15,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+
+  controlButton: {
+    transition: 'all 0.3s ease-in-out',
+    margin: 8,
+  },
 });
